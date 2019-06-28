4eb91705a430cf710ba5fde7879de13a8d3f6bca
Reina Liew <notalk_0@hotmail.com>
2019-06-28 23:49:09 +0800

    done

---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={style.recipe}>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient, index) => (
+          <li key={title + index}>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>{calories}</p>
+      <img className={style.image} src={image} alt={title} />
+    </div>
+  );
+};
+
+export default Recipe;
